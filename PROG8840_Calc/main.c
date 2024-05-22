31c28a4a47e4d5a1cf4518a6630c62ef263cd218
Harix034 <harix877@outlook.com>
2024-05-22 09:54:53 -0400

    Added break andreturn main.c

--- PROG8840_Calc/main.c
+++ PROG8840_Calc/main.c
@@ -6,51 +6,61 @@ void add();
 void subtract();
 void multiply();
 
+int main() {  
+    int inputNum;
 
-void main() {
+    while (1) {  
+        printWelcomeMenu();
+        printOptions();
 
-	printWelcomeMenu();
+        printf("Enter operation number (0 to exit): ");
+        scanf_s("%d", &inputNum);  
 
-	printOptions();
+        if (inputNum == 0) {
+            break;  
+        }
 
-	int inputNum;
+        switch (inputNum) {
+            case 1:
+                add();
+                break;  
+            case 2:
+                subtract();
+                break;  
+            case 3:
+                multiply();
+                break;  
+            default:
+                printf("Invalid operation number.\n");
+                break;  
+        }
+    }
 
-	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
-
-	switch (inputNum)
-	{
-	case 1:
-		add();
-	case 2:
-		subtract();
-	case 3:
-		multiply();
-	}
-
+    return 0;  
 }
 
 void printWelcomeMenu() {
-	printf(" **********************\n");
-	printf("**   Welcome to the   **\n");
-	printf("**   PROG8840 Calculator   **\n");
-	printf(" **********************\n");
+    printf(" **********************\n");
+    printf("**   Welcome to the   **\n");
+    printf("**   PROG8840 Calculator   **\n");
+    printf(" **********************\n");
 }
 
 void printOptions() {
-	printf("1. Add\n");
-	printf("2. Subtract\n");
-	printf("3. Multiply\n");
+    printf("1. Add\n");
+    printf("2. Subtract\n");
+    printf("3. Multiply\n");
+    printf("0. Exit\n");  
 }
 
 void add() {
-	double num1, num2, result;
-	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
-	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
-	result = num1 + num2;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+    double num1, num2, result;
+    printf("Enter the first value: ");
+    scanf_s("%lf", &num1);
+    printf("Enter the second value: ");
+    scanf_s("%lf", &num2);
+    result = num1 + num2;
+    printf("%lf + %lf = %lf\n", num1, num2, result);
 }
 
 void subtract() {
